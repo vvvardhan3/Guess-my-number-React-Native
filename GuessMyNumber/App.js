d8027038deb628096da7e4a01fbba3d7e22926ed
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-10-02 15:47:42 +0530

    Styling start screen buttons and TextInput

--- GuessMyNumber/App.js
+++ GuessMyNumber/App.js
@@ -1,13 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StyleSheet } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return (
-    <StartGameScreen />
-  );
+  return <StartGameScreen />;
 }
 
-const styles = StyleSheet.create({
-  
-});
+const styles = StyleSheet.create({});
--- GuessMyNumber/screens/StartGameScreen.js
+++ GuessMyNumber/screens/StartGameScreen.js
@@ -1,14 +1,36 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
-    return (
-        <View>
-            <TextInput />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>
-    )
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} maxLength= {2} />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: 'center',
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: 'bold'
+  }
+});
 
 export default StartGameScreen;
